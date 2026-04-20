6947fb1913dbd9f517dd2d82b8799dc2245b2c04
jmsto <jmsto@DESKTOP-3DHENI9>
2026-04-20 13:15:37 -0400

    Added checkout page

--- JCL_Website/Controllers/CartController.cs
+++ JCL_Website/Controllers/CartController.cs
@@ -24,6 +24,8 @@ namespace JCL_Website.Controllers
                 ReturnUrl = returnUrl
             });
         }
+        public ViewResult Checkout() => View(new Order());
+        
         public RedirectToActionResult AddToCart(int productId, string returnUrl)
         {
             Product product = repository.Products
