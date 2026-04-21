6bb3ca6037811cc4cde8dcd246525636f30e0c47
Luke Hinson <lmhinson55s@gmail.com>
2026-04-20 20:47:29 -0400

    Added controllers for my pages.

---
+++ JCL_Website/Controllers/BackgroundController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JCL_Website.Controllers
+{
+    public class BackgroundController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
